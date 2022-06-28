6bf338074b0a78a4a63157f2248bab80b174f9ea
Stewart Campbell <stewartcuk@gmail.com>
2022-06-28 17:11:33 +0200

    adding a concern to make more elegant json responses

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ProductsController < ApplicationController
       def index
-        render json: Product.all
+        json_response(Product.all)
       end
     end
   end
