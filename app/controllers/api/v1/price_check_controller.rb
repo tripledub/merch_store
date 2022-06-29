60b21bff691a6df5554b703d71b421cffca7aeb5
Stewart Campbell <stewartcuk@gmail.com>
2022-06-29 19:51:41 +0200

    gracefull handle malformed parameters

--- app/controllers/api/v1/price_check_controller.rb
+++ app/controllers/api/v1/price_check_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class PriceCheckController < ApplicationController
       def create
-        # debugger
         PriceCheck.new(items: query_params[:items]).total.tap do |total|
           json_response(total)
         end
@@ -11,7 +10,6 @@ module Api
       private
 
       def query_params
-        # puts params
         params.require(:query).permit(items: {}).to_h || {}
       end
     end
--- spec/requests/api/v1/price_check_spec.rb
+++ spec/requests/api/v1/price_check_spec.rb
@@ -5,30 +5,46 @@ RSpec.describe 'Api::V1::PriceCheck', type: :request do
   let!(:tshirt) { create(:product, code: 'TSHIRT', name: 'Reedsy T-Shirt', price: 15.00) }
   let!(:hoodie) { create(:product, code: 'HOODIE', name: 'Reedsy Hoodie', price: 20.00) }
   let(:params)  { {} }
-  let(:items) { {} }
+  let(:items)   { {} }
 
   describe 'POST /api/v1/price_check' do
     before { post '/api/v1/price_check', params: }
 
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
-    end
+    context 'with valid params' do
+      let(:params) { { query: { items: } } }
 
-    let(:params) { { query: { items: } } }
+      context '1 of each item' do
+        let(:items) { { 'MUG' => 1, 'TSHIRT' => 1, 'HOODIE' => 1 } }
 
-    context '1 of each item' do
-      let(:items) { { 'MUG' => 1, 'TSHIRT' => 1, 'HOODIE' => 1 } }
+        it 'returns the correct price' do
+          expect(json).to eq(41.0)
+        end
 
-      it 'returns the correct price' do
-        expect(json).to eq(41.0)
+        it 'returns http success' do
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context '2 mugs 4 tshirts, 1 hoodie' do
+        let(:items) { { 'MUG' => 2, 'TSHIRT' => 4, 'HOODIE' => 1 } }
+
+        it 'returns the correct price' do
+          expect(json).to eq(92.0)
+        end
+
+        it 'returns http success' do
+          expect(response).to have_http_status(:success)
+        end
       end
     end
 
-    context '2 mugs 4 tshirts, 1 hoodie' do
-      let(:items) { { 'MUG' => 2, 'TSHIRT' => 4, 'HOODIE' => 1 } }
+    context 'with missing or invalid params' do
+      it 'returns http status code 422' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
 
-      it 'returns the correct price' do
-        expect(json).to eq(92.0)
+      it 'gives a useful message' do
+        expect(json['message']).to eq('param is missing or the value is empty: query')
       end
     end
   end
