8ad4f7d2d958ca5a18b7265ea733e12e208f1c74
Stewart Campbell <stewartcuk@gmail.com>
2022-06-29 23:34:30 +0200

    adding seeds for testing with cURL

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Product.create(code: 'MUG', name: 'Reedsy Mug', price: 6.00)
-Product.create(code: 'TSHIRT', name: 'Reedsy T-Shirt', price: 15.00)
+Product.create(code: 'MUG', name: 'Reedsy Mug', price: 6.00).tap do |mug|
+  discount = 0
+  quantity = 0
+  while quantity < 150
+    discount += 2
+    quantity += 10
+
+    DiscountRule.create(product: mug, code: mug.code, quantity:, discount:)
+  end
+end
+
+Product.create(code: 'TSHIRT', name: 'Reedsy T-Shirt', price: 15.00).tap do |tshirt|
+  DiscountRule.create(product: tshirt, code: tshirt.code, quantity: 3, discount: 30)
+end
+
 Product.create(code: 'HOODIE', name: 'Reedsy Hoodie', price: 20.00)
