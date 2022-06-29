4fe3d90f8f48096ea8c9d5c71980d4f90d7a42f2
Stewart Campbell <stewartcuk@gmail.com>
2022-06-29 23:24:35 +0200

    Refactor: Memoize all rules for matching products to save exess DB queries

--- app/services/price_check.rb
+++ app/services/price_check.rb
@@ -19,14 +19,28 @@ class PriceCheck
 
   private
 
+  def discount_rule(code:, qty:)
+    rules = discount_rules.select do |rule|
+      rule['code'] == code && rule['quantity'] <= qty.to_i
+    end
+
+    rules.sort_by.with_index do |h, i|
+      [-h['quantity'], i]
+    end.first
+  end
+
+  def discount_rules
+    @discount_rules ||= DiscountRule.select(
+      :code, :quantity, :discount
+    ).where(code: items.keys).to_a.map(&:serializable_hash)
+  end
+
   def price_with_discount(code:, qty:)
-    discount = DiscountRule.where(code:, quantity: ..qty).order('quantity DESC').first
     original_price = products[code]
+    rule = discount_rule(code:, qty:)
 
-    # debugger
-
-    if discount
-      discount_rate = discount.discount
+    if rule
+      discount_rate = rule['discount']
       (original_price - (original_price * discount_rate / 100)) * qty.to_i
     else
       original_price * qty.to_i
