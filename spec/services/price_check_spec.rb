02821adbb2f176053ff7989dacaef1593618a7c7
Stewart Campbell <stewartcuk@gmail.com>
2022-06-29 16:03:45 +0200

    can now total combination of items

--- spec/services/price_check_spec.rb
+++ spec/services/price_check_spec.rb
@@ -13,5 +13,25 @@ RSpec.describe 'PriceCheck', type: :service do
     context 'when there are no items' do
       it { is_expected.to eq(0.00) }
     end
+
+    context 'when there is one of each item' do
+      let(:items) { { 'MUG' => 1, 'TSHIRT' => 1, 'HOODIE' => 1 } }
+      it { is_expected.to eq(41.00) }
+    end
+
+    context '2 mugs 1 tshirt' do
+      let(:items) { { 'MUG' => 2, 'TSHIRT' => 1 } }
+      it { is_expected.to eq(27.00) }
+    end
+
+    context '3 mugs 1 tshirt' do
+      let(:items) { { 'MUG' => 3, 'TSHIRT' => 1 } }
+      it { is_expected.to eq(33.00) }
+    end
+
+    context '2 mugs 4 tshirts, 1 hoodie' do
+      let(:items) { { 'MUG' => 2, 'TSHIRT' => 4, 'HOODIE' => 1 } }
+      it { is_expected.to eq(92.00) }
+    end
   end
 end
